e3720b583c0eef3aebca9d5994bf8f9b6af484d6
Marcel Solle <marcel.solle@ionos.com>
2021-08-25 13:14:07 +0200

    feat: update dots svg

--- src/components/Dots.vue
+++ src/components/Dots.vue
@@ -1,13 +1,15 @@
 <template>
-  <svg ref="svg" width="100%" height="100%" viewBox="0 0 50 50">
-    <circle
-      v-for="dot in dots"
-      :cx="dot.x"
-      :cy="dot.y"
-      :r="size"
-      fill="#1b365c"
-    />
-  </svg>
+  <div ref="wrapper">
+    <svg ref="svg" :width="width" :height="height" :viewBox="viewBox">
+      <circle
+        v-for="dot in dots"
+        :cx="dot.x"
+        :cy="dot.y"
+        :r="size"
+        fill="#1b365c"
+      />
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -16,7 +18,7 @@ export default {
   props: {
     size: {
       type: Number,
-      default: 1,
+      default: 2,
     },
     offsetX: {
       type: Number,
@@ -30,25 +32,41 @@ export default {
   data() {
     return {
       dots: [],
+      width: 1,
+      height: 1,
+      viewX: 100,
+      viewY: 100,
     };
+  },
+  computed: {
+    viewBox() {
+      return `0 0 ${this.width} ${this.height}`;
+    },
   },
   methods: {
     init() {
-      for (let i = 0; i < 15; i++) {
+      this.updateBounds()
+      for (let i = 0; i < 50; i++) {
         this.dots.push(this.createDot(i * 2 * this.size, i * 2 * this.size));
       }
     },
     createDot(x = 0, y = 0) {
-      return { x: this.offsetX + x, y: this.offsetY + y };
+      return {
+        x: `${(x) + this.offsetX}%`,
+        y: `${(y) + this.offsetY}%`,
+      };
     },
+    updateBounds() {
+      this.width = this.$refs.wrapper.offsetWidth;
+      this.height = this.$refs.wrapper.offsetHeight;
+    }
   },
   mounted() {
     this.init();
-    console.log(this.$refs.svg);
+    window.addEventListener('resize', this.updateBounds)
   },
+  destroyed() {
+    window.removeEventListener('resize', this.updateBounds)
+  }
 };
 </script>
-
-<style scoped>
-
-</style>
