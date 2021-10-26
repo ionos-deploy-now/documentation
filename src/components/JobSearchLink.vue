9f7965ae1a1a83ee45705b6d150eb5bd5d93b560
Marcel Solle <marcel.solle@ionos.com>
2021-10-26 16:22:57 +0200

    feat: change job search link to english

--- src/components/JobSearchLink.vue
+++ src/components/JobSearchLink.vue
@@ -4,12 +4,12 @@
     <form
       ref="jobSearchForm"
       method="post"
-      action="https://jobs.ionos.de/karriere/jobs/jobsuche"
+      action="https://jobs.ionos.de/career/all-jobs/job-search"
       target="_blank"
       class="hidden"
     >
       <input type="hidden" name="tx_cisocareer_jobsearchform[textSearch]" value="[Deploy Now]">
-      <input type="hidden" name="tx_cisocareer_jobsearchform[categories][]" value="11">
+      <input type="hidden" name="tx_cisocareer_jobsearchform[categories][]" value="">
       <input type="hidden" name="tx_cisocareer_jobsearchform[locationName][]" value="Karlsruhe">
     </form>
   </a>
