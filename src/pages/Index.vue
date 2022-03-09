70771620cdf36c662b7e58bf9423aa463591e46e
Robert <robert.schleinhege@ionos.com>
2022-03-09 15:07:35 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,7 +27,7 @@ export default {
 
   metaInfo: metaInfo({
     title: 'Documentation',
-    description: 'Build, stage & deploy static sites, single page applications and PHP apps directly via GitHub to IONOS infrastructure.',
+    description: 'Build, stage & deploy static sites, single page applications and PHP apps directly via GitHub to IONOS infrastructure. Supports Hugo, Gatsby, Laravel, Symfony and many more.',
   }),
 };
 </script>
