e3cc9da9230eca7c497e4ad18935b32824c5fa61
Robert <robert.schleinhege@ionos.com>
2022-03-09 14:55:56 +0100

    Update Index.vue
    Changed meta description

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,7 +27,7 @@ export default {
 
   metaInfo: metaInfo({
     title: 'Documentation',
-    description: 'Deploy Now is a platform for building and hosting Static Site Generators and Single Page Applications on inhouse engineered IONOS infrastructure.',
+    description: 'Build, stage & deploy static sites, single page applications and PHP apps directly via GitHub to IONOS infrastructure.',
   }),
 };
 </script>
