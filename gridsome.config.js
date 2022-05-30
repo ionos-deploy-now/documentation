06a5385e68726f8dc432caebca8ebee33a5d82c9
Robert <robert.schleinhege@ionos.com>
2022-05-30 13:52:29 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -76,6 +76,7 @@ module.exports = {
               '/docs/domain-tls/',
               '/docs/deployment-viewer/',
               '/docs/visitor-statistics/',
+              '/docs/cronjobs/',
             ],
           },
           {
